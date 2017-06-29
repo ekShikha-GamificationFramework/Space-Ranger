6ee0443a9aef5e5750069ff643b96763169ab0e4
easy-1 <rishabhg1997@gmail.com>
2017-06-29 11:50:32 +0530

    update

--- js/loadQuestions.js
+++ js/loadQuestions.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', loadQuestions, false);
 var xhttp;
 function loadQuestions() {
+    var urlParams = parseURLParams(window.location.href);
+    var x = 1, y = 1;
+    if(urlParams) {
+        x = urlParams["x"][0];
+        y = urlParams["y"][0];
+    }
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -14,6 +20,27 @@ function loadQuestions() {
             }
         }
     };
-    xhttp.open("GET", "questions.json");
+    xhttp.open("GET", "questions" + x + y + ".json");
     xhttp.send();
+}
+
+//function to parse the parameters passed in url
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
 }
